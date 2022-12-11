92b147f1c0c1da815e12a0d916026aaa01f52637
KristalMejia <20210046@itla.edu.do>
2022-12-11 17:11:51 -0400

    first commit

---
+++ questions.py
@@ -0,0 +1,37 @@
+# Preguntas 
+# y repuestas del chatbot para atender preguntas acerca de las mujeres 
+
+
+data = {'user': []}
+
+data['user'].append({
+    'responseBot': 'Hey',
+    'listWords' : ['nombre', 'puedo', 'quieres']})
+
+data['user'].append({
+    'responseBot': 'Hey, ¿En que puedo ayudarte?',
+    'listWords': ["Buenas", "Buen", "hello", "hola", "saludos", "días", "tardes", "Buenos"]})
+
+data['user'].append({
+    'responseBot': 'Estaremos respondiendo tus preguntas acerca de temas femeninos', 
+    'listWords': ["Período", "Mujer", "Niña", "Quiero", "saber", "Acerca", "temas", "femeninos", "el periodo" ]})
+
+data['user'].append({
+    'responseBot': 'puedes tener tu primera menstruación después de la pubertad',
+    'listWords': ["Cuándo", "primer", "perido", "tendre", "menstruación"]})
+
+data['user'].append({
+    'responseBot': 'normalemte son sensibles, por su manera de amar' 'es una pregunta que se hacen hasta ellas mismas' 'son importantes por que las necesitamos en muchos aspectos de la vida',
+    'listWords': ["las","Por qué", "mujeres", "son", "complicadas", "sensibles", "importantes"]})
+
+data['user'].append({
+    'responseBot': 'eso dependerá de tu cuerpo pero puede durar  de 24 a 28 días',
+    'listWords': ["cuanto", "me", "puede", "durar", "dura", "un", "ciclo"]})
+
+data['user'].append({
+    'responseBot': 'mi creadora dice que eso es un amarre',
+    'listWords': ["porque", "preguntan", "acerca", "del", "signo zodiacal"]})
+
+data['user'].append({
+    'responseBot': 'las mujeres muchas veces preguntan las cosas, que algún día desean que le pregunten o que le regalen',
+    'listWords': ['color', 'favorito', 'flores', 'comida']})
